f47089ffa397aff751ed4bb02d4cb6be191db68c
ChiefLopes <chieflopesekede@gmail.com>
2023-06-19 16:32:10 +0100

    added javascript functionalities like setInterval

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 const progressBar = document.querySelector(".progress-bar");
 const btn = document.querySelector("button");
 const input = document.querySelector("input");
+const value = document.querySelector(".value");
+let count = 0;
+
+btn.addEventListener("click", bar)
+
+function bar() {
+   const progress = setInterval(() => {
+       count++;
+       value.textContent = `${count}%`
+       if (count == input.value) {
+           clearInterval(progress);
+       }
+       console.log(count);
+   });
+}
+
+
