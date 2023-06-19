d768a74474f8f632615025cf712e6733a7f4316d
ChiefLopes <chieflopesekede@gmail.com>
2023-06-19 16:53:19 +0100

    added javascript functionalities

--- main.js
+++ main.js
@@ -9,11 +9,12 @@ btn.addEventListener("click", bar)
 function bar() {
    const progress = setInterval(() => {
        count++;
-       value.textContent = `${count}%`
+       value.textContent = `${count}%`;
+       progressBar.style.backgroundColor = `conic-gradient(#880bea ${120}deg, #ededed 0%)`;
        if (count == input.value) {
            clearInterval(progress);
        }
-       console.log(count);
+      
    });
 }
 
