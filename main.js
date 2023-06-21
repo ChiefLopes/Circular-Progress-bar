83cce06cd49d47a33e0ed38bb2335446dcd61455
ChiefLopes <chieflopesekede@gmail.com>
2023-06-21 20:03:07 +0100

    added a feature that makes the bar blink red when no number is entered

--- main.js
+++ main.js
@@ -3,19 +3,34 @@ const btn = document.querySelector("button");
 const input = document.querySelector("input");
 const value = document.querySelector(".value");
 let count = 0;
+let colors = ["red", "#fff"];
 
-btn.addEventListener("click", bar)
+btn.addEventListener("click", bar);
 
 function bar() {
-   const progress = setInterval(() => {
-       count++;
-       value.textContent = `${count}%`;
-       progressBar.style.background = `conic-gradient(#880bea ${count}deg, #ededed 0%)`;
-       if (count == input.value) {
-           clearInterval(progress);
-       }
-      
-   });
+  const progress = setInterval(() => {
+    count++;
+    value.textContent = `${count}%`;
+    progressBar.style.background = `conic-gradient(#880bea ${
+      count * 3.6
+    }deg, #ededed 0%)`;
+    if (count == input.value) {
+      clearInterval(progress);
+      input.value = "";
+    } else if (input.value <= 0) {
+      value.textContent = "no number entered";
+      value.style.fontSize = "18px";
+      value.style.color = "red";
+      var interval = setInterval(function () {
+        let currentColor = progressBar.style.background;
+        let newColor = currentColor === colors[0] ? colors[1] : colors[0];
+        progressBar.style.background= newColor;
+      }, 10);
+        
+        var clearTime = 5000; // Time in milliseconds to clear the interval
+        setTimeout(function () {
+          clearInterval(interval);
+        }, clearTime);
+    } 
+  });
 }
-
-
