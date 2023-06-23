a31f08e0a12d742185f9078d79f48acad347da7e
ChiefLopes <chieflopesekede@gmail.com>
2023-06-23 07:45:14 +0100

    modified main.js

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ let colors = ["red", "#fff"];
 
 btn.addEventListener("click", bar);
 
+// function reset() {
+//   value.textContent = "0%";
+//   value.fontSize = "0px";
+// }
+
+// console.log(reset);
+
 function bar() {
   const progress = setInterval(() => {
     count++;
@@ -17,25 +24,37 @@ function bar() {
     if (count == input.value) {
       clearInterval(progress);
       input.value = "";
-    } else if (input.value <= 0) {
-      value.textContent = "no number entered";
-      value.style.fontSize = "18px";
-      value.style.color = "red";
-      var interval = setInterval(function () {
-        let currentColor = progressBar.style.background;
-        let newColor = currentColor === colors[0] ? colors[1] : colors[0];
-          progressBar.style.background = newColor;
-          console.log(newColor);
-      }, 10);
-        
-        var clearTime = 1000; // Time in milliseconds to clear the interval
-        setTimeout(function () {
-            clearInterval(interval);
-            
-            if (clearTime == 1000) {
-               progressBar.style.background = 'green'
-            }
-        }, clearTime);
-    } 
+    } else if (count > 100) {
+       value.textContent = `${count}% limit ` ;
+    }
   });
 }
+
+
+// const greet = setInterval(() => {
+//   console.log("Lopes");
+// }, 5000);
+
+// setTimeout(() => {
+//   clearInterval(greet)
+// }, 50000);
+
+// else if (input.value <= 0) {
+//       value.textContent = "no number entered";
+//       value.style.fontSize = "18px";
+//       value.style.color = "red";
+
+//       var colorChangeCount = 0;
+//       var interval = setInterval(function () {
+//         let currentColor = progressBar.style.background;
+//         let newColor = currentColor === colors[0] ? colors[1] : colors[0];
+//         progressBar.style.background = newColor;
+//         colorChangeCount++;
+
+//         if (colorChangeCount === 3) {
+//           progressBar.style.background = "red";
+//           clearInterval(interval);
+//         }
+//       }, 1000);
+//     }
+
